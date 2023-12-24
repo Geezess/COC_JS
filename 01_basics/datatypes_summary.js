d66de2a7f793e1a6f0027715f1a0adb02a1c0e1f
Gaurav Swami <112553634+Geezess@users.noreply.github.com>
2023-12-24 13:07:16 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,32 @@
+//  Primitive 7 types
+
+// String
+// Number
+// Boolean
+// Null
+// Undefined
+// Symbol
+// BigInt
+
+
+// Reference/Non-Primitive type
+
+// Array
+// Objects
+// Functions
+
+// Inn sabka type object hi aata hai non primitive ka
+
+const heroes = ["Superman","Spiderman","Ironman"]
+
+console.log(heroes[1])
+
+let bio  = {
+    name : "Gaurav",
+    age : 21
+}
+
+console.log(bio)
+
+console.log(typeof bio)
+console.log(typeof heroes)
