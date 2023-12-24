f496d6081dd0c837f4abec9ebed1245a9658c6a2
Gaurav Swami <112553634+Geezess@users.noreply.github.com>
2023-12-24 11:38:07 +0000

    added variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144354
+let accountEmail = "gaurav@gmail.com"
+var accountPassword = "123456"
+accountHolder = "Gaurav"
+let accountPin;
+
+// accountId = 12343322
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountHolder,accountPin])
