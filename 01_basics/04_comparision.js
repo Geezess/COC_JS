75e13d4f1abe3c4c419d6487718ddd0259075738
Gaurav Swami <112553634+Geezess@users.noreply.github.com>
2023-12-24 12:53:05 +0000

    learnt comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1)
+console.log("1" == 1)
+console.log("1" === 1) //strict comparision mai datatype bhi same hona chaiye
+console.log("G" > 1)
+// javascript thoda ajjeb hai isliye compare hamesha same datatypes ka kro
+ 
