66cb17acb5a5f640cdff4dc7d28d704596d790e4
Gaurav Swami <112553634+Geezess@users.noreply.github.com>
2023-12-24 11:51:03 +0000

    learn datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"
+
+console.log(3+3)
+
+let name = "Gaurav"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 pow 53
+// string => ""
+// boolean => true/false
+// null => standalone value / empty
+// undefined => variable with no assigned value
+// symbol => uniqueness
+
+// object
+
+console.log(typeof "Gaurav")
+console.log(typeof 25)
+console.log(typeof undefined)
+console.log(typeof null) // null is object datatype
+
+
