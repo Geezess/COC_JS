fbd3e8b7710357bf326d2310b83a664f02fab0c0
Gaurav Swami <112553634+Geezess@users.noreply.github.com>
2023-12-24 19:22:34 +0000

    divided code into two files

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -30,34 +30,4 @@ console.log("Original Array ", myArr);
 
 
 
-const marvel_heroes = ["spiderman", "thor", "ironman"];
-const dc_heroes = ["batman", "superman", "aquaman"];
 
-marvel_heroes.push(dc_heroes)
-console.log(marvel_heroes)
-marvel_heroes.pop()
-
-const allHeroes = marvel_heroes.concat(dc_heroes)
-console.log(allHeroes)
-
-// another way to concate multiple arrays is using spread(...)
-
-const allNewHeroes = [...marvel_heroes,...dc_heroes]
-console.log(allNewHeroes)
-
-
-
-// conversion to Array
-
-console.log(Array.isArray("Gaurav"))
-console.log(Array.from("Gaurav")) //string to array
-
-
-
-// making arrays of given data
-
-let a = 100
-let b = 200
-let c = 300
-
-console.log(Array.of(a,b,c))
