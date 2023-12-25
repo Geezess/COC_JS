e1998fb4ee894a677ef1a82b1cf89115f43bb79f
Gaurav Swami <112553634+Geezess@users.noreply.github.com>
2023-12-25 16:17:05 +0000

    learnt switch

---
+++ control_flow/switch.js
@@ -0,0 +1,48 @@
+// Switch
+
+const month = 7
+
+switch (month){
+  case 1:
+    console.log("January")
+    break;  
+  case 2:
+    console.log("February")
+    break;  
+  case 3:
+    console.log("March")
+    break;  
+  case 4:
+    console.log("April")
+    break;  
+  case 5:
+    console.log("May")
+    break;  
+
+    default:
+        console.log("No month matched")
+}
+
+
+const monthString = "feb"
+
+switch (monthString){
+    case "jan":
+      console.log("January")
+      break;  
+    case "feb":
+      console.log("February")
+      break;  
+    case "mar":
+      console.log("March")
+      break;  
+    case "apr":
+      console.log("April")
+      break;  
+    case "may":
+      console.log("May")
+      break;  
+  
+      default:
+          console.log("No month matched")
+  }
