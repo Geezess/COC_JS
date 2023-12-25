6e487ff79f10de3e5f0ec756fb905ab4af8b92d1
Gaurav Swami <112553634+Geezess@users.noreply.github.com>
2023-12-25 16:16:53 +0000

    learnt if else

---
+++ control_flow/if-else.js
@@ -0,0 +1,37 @@
+// IF
+
+const isUserLoggedIn = true
+if(isUserLoggedIn){
+      
+}
+
+let a = 50 
+if(a == "50"){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
+
+// when we use === woh datatype bhi check krta hai strictly.
+if(a === "50"){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
+
+if(a != "50"){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
+
+// same isme strictly not equal datatype bhi check hoga.
+if(a !== "50"){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
